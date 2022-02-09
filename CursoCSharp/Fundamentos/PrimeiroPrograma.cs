dc5b904eded400cb866765b6e950073910d630f4
Sergio <Sergio@DESKTOP-I217KMQ>
2022-02-09 12:52:43 -0300

    início

---
+++ CursoCSharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CursoCSharp.Fundamentos {
+    class PrimeiroPrograma {
+        static void Main() {
+            System.Console.Write("Primeiro");
+            Console.WriteLine("Programa");
+            Console.WriteLine("Terminou");
+        }
+
+    }
+}
