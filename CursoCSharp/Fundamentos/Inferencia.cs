5875e3cad26c00732b1c4c0245a817cfb7670382
Sergio <Sergio@DESKTOP-I217KMQ>
2022-02-15 09:15:36 -0300

    continuação

--- CursoCSharp/Fundamentos/Inferencia.cs
+++ CursoCSharp/Fundamentos/Inferencia.cs
@@ -8,6 +8,20 @@ namespace CursoCSharp.Fundamentos {
      class Inferencia {
 
         public static void Executar(){
+            var nome = "Leonardo";
+            // nome = 123;
+            Console.WriteLine(nome);
+
+            // int idade;
+            var idade = 32;
+            Console.WriteLine(idade);
+
+            int a;
+            a = 3;
+
+            int b = 2;
+
+            Console.WriteLine(a + b );
 
 
         }
