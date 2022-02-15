a1307fba60952491301d8b0bfb21396694ab88bf
Sergio <sergiolourencodeazevedo@gmail.com>
2022-02-15 17:06:36 -0300

    .

--- CursoCSharp/Fundamentos/Interpolacao.cs
+++ CursoCSharp/Fundamentos/Interpolacao.cs
@@ -5,6 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CursoCSharp.Fundamentos {
-    internal class Interpolacao {
+    class Interpolacao {
+
+        public static void Executar() {
+            string nome = "Nootbook Gamer";
+            var marca = "Dell";
+            double preco = 5800.00;
+
+            Console.WriteLine("o " + nome + " da marca " 
+                + marca + " custa " + preco + " . ");
+
+            Console.WriteLine("O {0} da marca {1} custa {2}.",
+                nome, marca, preco);
+
+            Console.WriteLine($"A marca {marca} é legal");
+            Console.WriteLine($"1 + 1 = {1+1} !");
+
+
+        }
     }
 }
--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -11,6 +11,7 @@ namespace CursoCSharp {
                 {"Comentários - Fundamentos ", Comentarios.Executar },
                 {"Váriaveis e Constantes - Fundamentos ", VariaveisEConstantes.Executar },
                 {"Inferência - Fundamentos ", Inferencia.Executar },
+                {"Interpolacao - Fundamentos", Interpolacao.Executar },
             });
 
             central.SelecionarEExecutar();
