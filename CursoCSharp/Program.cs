335b8be5527630571f9048de0a5bf9aacc96a8ba
Sergio <sergiolourencodeazevedo@gmail.com>
2022-02-16 13:02:59 -0300

    Formatando Números

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -14,6 +14,7 @@ namespace CursoCSharp {
                 {"Interpolação - Fundamentos", Interpolacao.Executar },
                 {"Notação Ponto - Fundamentos", NotacaoPonto.Executar },
                 {"Lendo Dados  - Fundamentos", LendoDados.Executar },
+                {"Formatando Número  - Fundamentos", FormatandoNumero.Executar },
             });
 
             central.SelecionarEExecutar();
