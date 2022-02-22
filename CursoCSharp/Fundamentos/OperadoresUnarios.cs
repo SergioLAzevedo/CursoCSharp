edf2335c9ddb8ea9c1e411eedc5ec4966158b932
Sergio <sergiolourencodeazevedo@gmail.com>
2022-02-22 13:06:40 -0300

    Operadores Unários

---
+++ CursoCSharp/Fundamentos/OperadoresUnarios.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+     class OperadoresUnarios {
+        public static void Executar() {
+            var valorNegativo = -5;
+            var numero1 = 2;
+            var numero2 = 3;
+            var boleano = true;
+
+            Console.WriteLine(-valorNegativo);
+            Console.WriteLine(!boleano);
+
+            numero1++;
+            Console.WriteLine(numero1);
+
+            --numero1;
+            Console.WriteLine(numero1);
+
+            Console.WriteLine(numero1++ == --numero2);   // esse tipo de codigo pode geral confusão, nao se deve coloca-lo
+            Console.WriteLine($"{numero1} {numero2}");
+
+
+        
+
+
+        }
+    }
+}
