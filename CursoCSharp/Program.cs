f1092a4951dfeea5d85abcc3e6f62a5a46e3c255
Sergio <sergiolourencodeazevedo@gmail.com>
2022-02-22 12:43:31 -0300

    Operadores de Atribuição

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -19,6 +19,7 @@ namespace CursoCSharp {
                 {"Operadores Aritmeticos - Fundamentos", OperadoresAritmeticos.Executar },
                 {"Operadores Relacionais - Fundamentos", OperadoresRelacionais.Executar },
                 {"Operadores Lógicos - Fundamentos", OperadoresLogicos.Executar },
+                {"Operadores de Atribuição - Fundamentos", OperadoresAtribuicao.Executar },
             });
 
             central.SelecionarEExecutar();
